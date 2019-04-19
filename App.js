3d07d3ffb107bb939f7fe3069931813400bde412
Chris Ward <cward@ddm.io>
2019-04-19 14:51:43 -0600

    players for each chord in scale

--- App.js
+++ App.js
@@ -3,34 +3,56 @@ import { Button, StyleSheet, Switch, Text, View } from 'react-native'
 import { Audio, Icon } from 'expo'
 import ChordButtons from './components/ChordButtons'
 
+const chords = ['I', 'ii', 'iii', 'IV', 'V', 'vi', 'viiᴼ'] // TODO: de-dupe this
+const semitones = ['c', 'cs', 'd', 'ds', 'e', 'f', 'fs', 'g', 'gs', 'a', 'as', 'b']
 const skipRestartThreshold = 500 // ms
 const statusUpdateInterval = 50  // ms
 
-const toc = [
+const samples = {
+  'c':  require('./assets/samples/db-c.wav'),
+  'cs': require('./assets/samples/db-cs.wav'),
+  'd':  require('./assets/samples/db-d.wav'),
+  'ds': require('./assets/samples/db-ds.wav'),
+  'e':  require('./assets/samples/db-e.wav'),
+  'f':  require('./assets/samples/db-f.wav'),
+  'fs': require('./assets/samples/db-fs.wav'),
+  'g':  require('./assets/samples/db-g.wav'),
+  'gs': require('./assets/samples/db-gs.wav'),
+  'a':  require('./assets/samples/db-a.wav'),
+  'as': require('./assets/samples/db-as.wav'),
+  'b':  require('./assets/samples/db-b.wav')
+}
+
+const tunes = [
   {
     "title": "Arkansas Traveler",
+    "key": "d",
     "difficulty": 2,
-    "audio": require('./assets/audio/arkansas_traveler.mp3'),
-    "map": require('./assets/audio/arkansas_traveler_map.json'),
+    "audio": require('./assets/tunes/arkansas_traveler.mp3'),
+    "map": require('./assets/tune_maps/arkansas_traveler.json'),
   },
   {
     "title": "Whiskey Before Breakfast",
+    "key": "d",
     "difficulty": 2,
-    "audio": require('./assets/audio/whiskey.mp3'),
-    "map": require('./assets/audio/whiskey_map.json'),
+    "audio": require('./assets/tunes/whiskey.mp3'),
+    "map": require('./assets/tune_maps/whiskey.json'),
   }
 ]
 
-
 export default class App extends React.Component {
+  sampler = {}
+  soundObject = null
+
   constructor(props) {
     super(props)
     this.chordMap = null
     this.state = {
       currentChord: 1,
-      currentTune: toc[0],
+      currentTune: tunes[0],
       currentTuneIndex: 0,
       isPlaying: false,
+      playBass: true,
       showCurrentChord: true,
       soundObjectDuration: null,
       soundObjectPosition: null,
@@ -38,7 +60,6 @@ export default class App extends React.Component {
   }
 
   componentWillMount() {
-    this._loadSampler()
     this._loadTune()
   }
 
@@ -72,11 +93,20 @@ export default class App extends React.Component {
         <View style={styles.controlsContainer}>
           <Text>Show current chord </Text>
           <Switch
-            onValueChange={this._handleShowCurrentChordSwitch}
+            onValueChange={() => this._handleToggle('showCurrentChord')}
             value={this.state.showCurrentChord}
           >
           </Switch>
         </View>
+        <View style={styles.controlsContainer}>
+          <Text>Play bass </Text>
+          <Switch
+            onValueChange={() => this._handleToggle('playBass')}
+            value={this.state.playBass}
+          >
+          </Switch>
+        </View>
+
         <Button
           title="Click"
           onPress={this._handleSamplerButtonPress}
@@ -86,15 +116,16 @@ export default class App extends React.Component {
     );
   }
 
-  async _getSoundObject(audio) {
-    this.soundObject = new Audio.Sound();
-    try {
-      this.soundObject.setOnPlaybackStatusUpdate(this._onPlaybackStatusUpdate)
-      await this.soundObject.loadAsync(audio)
-      this.soundObject.setProgressUpdateIntervalAsync(statusUpdateInterval)
-    } catch (error) {
-      console.log(error)
-    }
+  _getScale = (key) => {
+    let semitoneSequence = [0, 2, 4, 5, 7, 9, 11]
+    let root = semitones.indexOf(key)
+
+    let scale = semitoneSequence.map((offset) => {
+      let index = (root + offset < semitones.length) ? root + offset : root + offset - semitones.length
+      return semitones[index]
+    })
+
+    return scale
   }
 
   _getTimestamp() {
@@ -111,7 +142,7 @@ export default class App extends React.Component {
   _handleBackPress = () => {
     if (this.soundObject != null) {
       if (this.state.soundObjectPosition < skipRestartThreshold) {
-        let index = (this.state.currentTuneIndex <= 0) ? toc.length - 1 : this.state.currentTuneIndex - 1
+        let index = (this.state.currentTuneIndex <= 0) ? tunes.length - 1 : this.state.currentTuneIndex - 1
         this._skipToTrack(index)
       } else {
         this.soundObject.setPositionAsync(0)
@@ -121,8 +152,12 @@ export default class App extends React.Component {
   }
 
   _handleNextPress = () => {
-    let index = (this.state.currentTuneIndex + 1 >= toc.length) ? 0 :this.state.currentTuneIndex + 1
+    let index = (this.state.currentTuneIndex + 1 >= tunes.length) ? 0 :this.state.currentTuneIndex + 1
     this._skipToTrack(index)
+  }
+
+  _handleToggle = (key) => {
+    this.setState({[key]: !this.state[key]})
   }
 
   _handlePlayPausePress = () => {
@@ -139,14 +174,10 @@ export default class App extends React.Component {
 
   _handleSamplerButtonPress = () => {
     if (this.sampler != null) {
-      this.sampler.replayAsync()
+      this.sampler['I'].replayAsync()
     } else {
       console.log('sampler is null')
     }
-  }
-
-  _handleShowCurrentChordSwitch = () => {
-    this.setState({showCurrentChord: !this.state.showCurrentChord})
   }
 
   _loadChordMap(map) {
@@ -154,10 +185,24 @@ export default class App extends React.Component {
     this.setState({currentChord: this.chordMap[0]['chord']})
   }
 
-  async _loadSampler() {
-    this.sampler = new Audio.Sound();
+  async _loadSamplers(key) {
+    let scale = this._getScale(key)
+
+    chords.forEach(async (chord, index) => {
+      this.sampler[chord] = new Audio.Sound()
+      try {
+        await this.sampler[chord].loadAsync(samples[scale[index]])
+      } catch (error) {
+        console.log(error)
+      }
+    })
+  }
+
+  async _loadSoundObject(audio) {
+    this.soundObject = new Audio.Sound()
     try {
-      await this.sampler.loadAsync(require('./assets/audio/Alesis-Fusion-Acoustic-Bass-C2.wav'))
+      this.soundObject.setOnPlaybackStatusUpdate(this._onPlaybackStatusUpdate)
+      await this.soundObject.loadAsync(audio)
       this.soundObject.setProgressUpdateIntervalAsync(statusUpdateInterval)
     } catch (error) {
       console.log(error)
@@ -165,8 +210,9 @@ export default class App extends React.Component {
   }
 
   _loadTune = () => {
-    this._getSoundObject(this.state.currentTune.audio)
+    this._loadSoundObject(this.state.currentTune.audio)
     this._loadChordMap(this.state.currentTune.map)
+    this._loadSamplers(this.state.currentTune.key)
   }
 
   _nextChord() {
@@ -190,7 +236,7 @@ export default class App extends React.Component {
     this.setState(
       {
         currentTuneIndex: index,
-        currentTune: toc[index],
+        currentTune: tunes[index],
         isPlaying: false
       },
       this._loadTune
@@ -217,3 +263,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   }
 })
+
+// samples by Carlos Vaquero https://freesound.org/people/Carlos_Vaquero/
